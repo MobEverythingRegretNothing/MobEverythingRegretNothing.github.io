797d5f00cda10ca0abbce405e2691fef87291e13
MobEverythingRegretNothing <43182872+MobEverythingRegretNothing@users.noreply.github.com>
2021-08-10 20:28:17 -0500

    Update script

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    alert("hi");
+    const urls = ["https://docs.google.com/forms/d/e/1FAIpQLSeKqmLijtKbG5u9-nVG6Cf1EwtWLWgAJJ12zCeCgqvxe9QgHQ/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSffMtmyN9qj5wevixRBs3e0cwOeADwgq-hIS1rp5km85OpUdg/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSeu5r8rcjDtpKBic9RuBodgWloWPnOX0fFHCDXpuxiVMYjo_w/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSdxuCr2ZHzHO-qdwe_bH5fxPU3PurvQ_LkhPAxi_oV0QlYfUA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSeGnTBb0yhhKvNo7mzMNB9VFsO7kUm5i4QB6GO3tgksLpLwqQ/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLScOJha0lgLP1xBrkuGGQVO-A-to0WDPe-Hv_9sQebe0Ciwc9g/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSf1phCuqSwk-3fudLcP9ucbiOy2fYYy6R-rb3rsqKr1d-TVtg/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSfTD2Cwk4QZr1-mPysLZXVdfSVM1rRSLfEr7PvgcrdjCNhTFQ/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSe6oSf4XOlOhDVWhDMoV7_KRKx5JpnCvZF93WZlwwAC6N1zyA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSfn1QckFDG8fPrc2HxUgLsMzynQtZa2k31OSSe1LRvvERX9FA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSfFzw_w9fmSZ94CoMJwb71Ro6Jck5NWxMa3t0Z-Lhhz2CDZCA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSfP7SwgvXp-6KWU8OvR9lKBU8QPHIQpxIb2X19nwdIShbhhmA/viewform?usp=sf_link"]
+    
+    window.location.href(urls[Math.random(12)]);
 })
